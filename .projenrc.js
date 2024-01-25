9281c720fd0e35f2186b85ad983f08a5527ca4ae
Armando Luja <aluja@amazon.com>
2024-01-25 14:56:09 -0800

    fix: remove unused lodash set

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@
 const { awscdk , javascript  } = require('projen');
 const { stringify } = require('yaml');
 
-const dependencies = ['fs-extra@10.0.0', 'lodash.get', 'lodash.set', 'uuid'];
+const dependencies = ['fs-extra@10.0.0', 'lodash.get', 'uuid'];
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Amazon Web Services',
   authorAddress: 'amplify-cli@amazon.com',
@@ -17,7 +17,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
     '@types/fs-extra@^8.1.1',
     '@types/jest',
     '@types/lodash.get',
-    '@types/lodash.set',
     '@types/node',
     '@types/uuid',
     'yaml',
